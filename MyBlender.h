2e0412e132da1eec75ed8f174c49494c145783e4
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-05-02 02:13:27 -0700

    Purify source code

--- MyBlender.h
+++ MyBlender.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/gpu/gpu.hpp>
 #include <opencv2/stitching/detail/blenders.hpp>
 
 namespace cv {
@@ -9,11 +10,16 @@ namespace detail {
 class MyBlender : public Blender
 {
 public:
-	//void prepare(const std::vector<Point> &corners, const std::vector<Size> &sizes);
+	MyBlender(int try_gpu = false);
     void prepare(Rect dst_roi);
-	//void setRoi(Rect roi);
     void feed(const Mat &img, const Mat &mask, Point tl);
     void blend(Mat &dst, Mat &dst_mask);
+
+protected:
+	bool use_gpu;
+	gpu::GpuMat gpuDst_;
+
+	void cudaFeed(const Mat &img, const Mat &mask, int dx, int dy);	// cuda version feed
 };
 
 } //namespace cv {
