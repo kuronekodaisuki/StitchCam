314c9613203de8ae391aa1103629c8ff57ffad3b
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-05-05 05:15:58 -0700

    add Blender.cu

--- MyBlender.cpp
+++ MyBlender.cpp
@@ -11,7 +11,7 @@ namespace detail {
 MyBlender::MyBlender(int try_gpu)
 {
 	use_gpu = false;
-/*	
+
 	if (try_gpu)
 	{
 		if (gpu::getCudaEnabledDeviceCount() > 0)
@@ -19,7 +19,6 @@ MyBlender::MyBlender(int try_gpu)
 			use_gpu = true;
 		}
 	}
-*/
 }
 
 void MyBlender::prepare(Rect dst_roi)
@@ -54,8 +53,8 @@ void MyBlender::feed(const Mat &img, const Mat &mask, Point tl)
 		for (int y = 0; y < img.rows; ++y)
 		{
 			const Point3_<uchar> *src_row = img.ptr<Point3_<uchar> >(y);
-			Point3_<uchar> *dst_row = dst_.ptr<Point3_<uchar> >(dy + y);
 			const uchar *mask_row = mask.ptr<uchar>(y);
+			Point3_<uchar> *dst_row = dst_.ptr<Point3_<uchar> >(dy + y);
 			uchar *dst_mask_row = dst_mask_.ptr<uchar>(dy + y);
 
 			for (int x = 0; x < img.cols; ++x)
@@ -66,6 +65,16 @@ void MyBlender::feed(const Mat &img, const Mat &mask, Point tl)
 			}
 		}
 	}
+}
+
+void MyBlender::feed(const gpu::GpuMat &img, const gpu::GpuMat &mask, Point tl)
+{
+	CV_Assert(img.type() == CV_8UC3);
+    CV_Assert(mask.type() == CV_8U);
+    int dx = tl.x - dst_roi_.x;
+    int dy = tl.y - dst_roi_.y;
+
+	cudaFeed(img, mask, dx, dy);
 }
 
 void MyBlender::blend(Mat &dst, Mat &dst_mask)
--- MyBlender.h
+++ MyBlender.h
@@ -3,6 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/gpu/gpu.hpp>
 #include <opencv2/stitching/detail/blenders.hpp>
+#include <opencv2/stitching/detail/util.hpp>
 
 namespace cv {
 namespace detail {
@@ -11,14 +12,21 @@ class MyBlender : public Blender
 {
 public:
 	MyBlender(int try_gpu = false);
+	void prepare(const vector<Point> &corners, const vector<Size> &sizes)
+	{
+		prepare(resultRoi(corners, sizes));
+	}
+
     void prepare(Rect dst_roi);
     void feed(const Mat &img, const Mat &mask, Point tl);
+	void feed(const gpu::GpuMat &img, const gpu::GpuMat &mask, Point tl);
     void blend(Mat &dst, Mat &dst_mask);
 
 protected:
 	bool use_gpu;
 	gpu::GpuMat gpuDst_;
 
+	void cudaFeed(const gpu::GpuMat &image, const gpu::GpuMat &mask, int dx, int dy); // cuda version feed
 	void cudaFeed(const Mat &img, const Mat &mask, int dx, int dy);	// cuda version feed
 };
 
