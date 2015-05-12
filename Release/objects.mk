0cf57a73ec27cba67b3037eb01d2daec01ec397d
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-05-13 06:06:54 +0900

    change for linux

--- Release/objects.mk
+++ Release/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lcudart -lopencv_calib3d -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_gpu -lopencv_highgui -lopencv_imgproc -lopencv_stitching -lopencv_video
+LIBS := -lcudart -lopencv_core -lopencv_gpu -lopencv_highgui -lopencv_imgproc -lopencv_stitching
 
