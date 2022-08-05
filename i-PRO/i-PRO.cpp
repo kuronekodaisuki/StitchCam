29177196d1b313f5649d9336bf00bcac6adb9d11
Hiro Matsuoka <kuronekodaisuki@me.com>
2022-08-05 18:53:47 +0900

    マルチカメラの個別映像取得

--- i-PRO/i-PRO.cpp
+++ i-PRO/i-PRO.cpp
@@ -3,7 +3,10 @@
 
 #include <opencv2/opencv.hpp>
 
-const char* camera1 = "rtsp://CyberAgent:FutureTechHub1234@192.168.131.22/Src/MediaInput/stream_1";
+const char* camera1 = "rtsp://CyberAgent:FutureTechHub1234@192.168.131.22/Src/MediaInput/stream_1/ch_1";
+const char* camera2 = "rtsp://CyberAgent:FutureTechHub1234@192.168.131.22/Src/MediaInput/stream_1/ch_2";
+const char* camera3 = "rtsp://CyberAgent:FutureTechHub1234@192.168.131.22/Src/MediaInput/stream_1/ch_3";
+const char* camera4 = "rtsp://CyberAgent:FutureTechHub1234@192.168.131.22/Src/MediaInput/stream_1/ch_4";
 
 int main()
 {
@@ -12,16 +15,24 @@ int main()
 
     if (cam1.open(camera1))
     {
-        for (bool loop = true; loop;)
+        if (cam2.open(camera2))
         {
-            cam1.read(image1);
-            cv::imshow("cam1", image1);
-
-            switch (cv::waitKey(10))
+            for (bool loop = true; loop;)
             {
-            case 'q':
-                loop = false;
-                break;
+                cam1.read(image1);
+                cam2.read(image2);
+                //cam3.read(image3);
+                //cam4.read(image4);
+
+                cv::imshow("cam1", image1);
+                cv::imshow("cam2", image2);
+
+                switch (cv::waitKey(10))
+                {
+                case 'q':
+                    loop = false;
+                    break;
+                }
             }
         }
     }
